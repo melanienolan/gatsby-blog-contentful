3de7a737218aba0e60867a8dfd3154d2693decb9
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-17 12:17:35 +0100

    create page title component

---
+++ src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PageTitle = ({ children }) => {
+  return (
+    <h2 className="my-12 text-purple-500 font-bold text-4xl text-center">
+      {children}
+    </h2>
+  )
+}
+
+export default PageTitle
