8a5c3f545e798dfd98a325563a91994ca4484872
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-17 16:38:57 +0100

    extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.js"],
   target: "relaxed",
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "2/3": "66.6666667%",
+      },
+      fontFamily: {
+        suez: ["Suez One", "sans-serif"],
+      },
+    },
   },
   variants: {},
   plugins: [],
