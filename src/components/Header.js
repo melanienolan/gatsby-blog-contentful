c14015f55f410cfe8b16083bda2bcc2e894dc60e
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-18 13:19:26 +0100

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
+import logoSrc from "../../static/logo.svg"
 
 const Header = () => {
   return (
-    <header className="p-4 bg-teal-500 text-white font-semibold flex justify-between">
-      <div>Logo</div>
-      <button className="sm:hidden">
-        <svg className="w-6 h-6 fill-current" viewBox="0 0 20 20">
-          <path
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-            fillRule="evenodd"
-          />
-        </svg>
-      </button>
-      {/* <nav
-            className="hidden px-6 absolute right-0 w-full"
-            style={{ top: "50px" }}
-          >
-            <ul className="flex flex-col items-center bg-white text-gray-700 shadow">
-              <li className="pt-4">
-                <Link to="/about">About</Link>
-              </li>
-              <li className="py-4">
-                <Link to="/articles">Articles</Link>
-              </li>
-              <li className="pb-4">
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav> */}
-      <nav className="hidden sm:block">
-        <ul className="flex text-white">
-          <li className="p-4">
-            <Link to="/about">About</Link>
+    <header className="p-4 bg-purple-500 text-white font-semibold uppercase tracking-widest flex justify-between font-suez">
+      <Link to="/" className="flex justify-center items-center">
+        <img src={logoSrc} alt="Logo" className="w-10 h-10" />
+      </Link>
+      <nav className="">
+        <ul className="flex items-center">
+          <li className="p-4 text-white">
+            <Link
+              to="/about"
+              className="border-current hover:border-b-2"
+              activeClassName="border-b-2"
+            >
+              About
+            </Link>
           </li>
-          <li className="p-4">
-            <Link to="/articles">Articles</Link>
+          <li className="p-4 text-white">
+            <Link
+              to="/blog"
+              className="border-current hover:border-b-2"
+              activeClassName="border-b-2"
+            >
+              Blog
+            </Link>
           </li>
-          <li className="p-4">
-            <Link to="/contact">Contact</Link>
+          <li className="p-4 text-white">
+            <Link
+              to="/contact"
+              className="border-current hover:border-b-2"
+              activeClassName="border-b-2"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
