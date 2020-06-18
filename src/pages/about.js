791e6ef0216439cfdca730b96bc846549466f129
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-18 13:25:03 +0100

    render description

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
 import Layout from "../components/Layout"
 import Container from "../components/Container"
 import PageTitle from "../components/PageTitle"
@@ -6,12 +8,20 @@ import usePerson from "../hooks/usePerson"
 
 export default () => {
   const person = usePerson()
-  console.log(person)
+
   return (
     <Layout>
       <Container>
         <PageTitle>About {person.name}</PageTitle>
-        <p>{person.bio}</p>
+        <div className="p-4 text-gray-800">
+          {documentToReactComponents(person.description, {
+            renderNode: {
+              [BLOCKS.PARAGRAPH]: (node, children) => (
+                <p className="text-gray-800 mb-4">{children}</p>
+              ),
+            },
+          })}
+        </div>
       </Container>
     </Layout>
   )
