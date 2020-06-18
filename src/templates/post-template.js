e6904607b3204ccbbac48ebf6b5963821df6cc3e
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-18 15:33:16 +0100

    render body text

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
 import Layout from "../components/Layout"
 import Container from "../components/Container"
 import PageTitle from "../components/PageTitle"
+import TextBox from "../components/TextBox"
 
 export const query = graphql`
   query($slug: String!) {
     blogPost: contentfulBlogPost(slug: { eq: $slug }) {
       slug
       title
-      description
+      body {
+        json
+      }
       image {
         fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
           ...GatsbyContentfulFluid_withWebp
@@ -23,16 +28,26 @@ export const query = graphql`
 const PostTemplate = ({ data: { blogPost } }) => (
   <Layout>
     <Container>
-      <div className="relative pb-2/3 mx-auto">
-        <Img
-          className="w-full h-full object-cover"
-          alt={blogPost.title}
-          style={{ position: "absolute" }}
-          fluid={blogPost.image.fluid}
-        />
+      <div className="max-w-screen-sm mx-auto">
+        <div className="relative pb-2/3">
+          <Img
+            className="w-full h-full object-cover"
+            alt={blogPost.title}
+            style={{ position: "absolute" }}
+            fluid={blogPost.image.fluid}
+          />
+        </div>
       </div>
       <PageTitle>{blogPost.title}</PageTitle>
-      <p className="p-4 text-gray-800">{blogPost.description}</p>
+      <TextBox>
+        {documentToReactComponents(blogPost.body.json, {
+          renderNode: {
+            [BLOCKS.PARAGRAPH]: (_node, children) => (
+              <p className="mb-4">{children}</p>
+            ),
+          },
+        })}
+      </TextBox>
       <Link to="/blog">
         <p className="m-4 text-purple-500 font-suez">&larr; Back to Blog</p>
       </Link>
